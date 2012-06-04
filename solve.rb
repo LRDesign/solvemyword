faf38fe06cb1d6b9d4efd4ada51d6e6f31f5bb6f
Evan Dorn <evan@lrdesign.com>
2012-06-03 19:55:22 -0700

    compute permutations

--- solve.rb
+++ solve.rb
@@ -18,21 +18,41 @@ get '/' do
 end
 
 get '/solve' do
-  pattern = params['pattern'].gsub(/_/,' ').chars
-  letters = params['letters'].chars
+  pattern = params['pattern'].gsub(/ /,'_').chars.to_a
+  letters = params['letters'].chars.to_a
   p pattern
   p letters
 
-  #possibilities = [].tap do |arr|
-    #letters.permutations.each do |lets|
+  permutations = letters.permutation.map do |lets|
+    p lets
+    n = 0
+    pattern.map do |ch|
+      if ch == "_"
+        n += 1
+        lets[n].to_s
+      else
+        ch.to_s
+      end
+    end.join
+  end.uniq
 
-    #end
-  #end
 
   "".tap do |result|
     result << "Params: #{params.inspect}"
     result << "<hr />"
     result << "You entered #{letters.inspect} for pattern #{pattern.inspect} <br>"
-    result << "Available dictionaries include #{dictionary.keys}"
+    result << "<hr />"
+    result << "Tested permutations: " + lines(permutations)
+    result << "<hr />"
+    result << "Available dictionaries include #{lines(dictionary.keys)}"
   end
+end
+
+
+def line(str)
+  "#{str}<br>"
+end
+
+def lines(array)
+  array.join("<br>")
 end
