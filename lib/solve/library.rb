a1572f343f32ff7898d1667f5afbe4da5d224cc2
Evan Dorn <evan@lrdesign.com>
2012-06-03 21:37:46 -0700

    added language selection to library

--- lib/solve/library.rb
+++ lib/solve/library.rb
@@ -1,18 +1,36 @@
 require 'solve/dictionary'
 module Solve
   class Library
+    LIMIT = 80
+    DEFAULT_OPTS = {
+      :language => 'american',
+      :limit => 70
+    }
+
     attr_accessor :dictionaries
 
     def initialize
       Dir.glob('dict/*-words.*') do |dict_file|
-        lang = dict_file.match(/(\w+)-words/)[1].to_s
-        size = dict_file.match(/\.(\d+)/)[1].to_i
-        if size < 90
+        level = dict_file.match(/\.(\d+)/)[1].to_i
+        if level <= LIMIT
           @dictionaries ||= []
           @dictionaries << Dictionary.new(dict_file)
         end
       end
     end
+
+    def matches_word?(word, opts = DEFAULT_OPTS)
+
+    end
+
+    def selected_dictionaries(opts = DEFAULT_OPTS)
+      languages = ['english'] + [*opts[:language]]
+
+      @dictionaries.select do |d|
+        languages.include?(d.language)
+      end
+    end
+
   end
 end
 
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -13,6 +13,52 @@ describe Solve::Library do
     end
   end
 
+  describe "selected_dictionaries" do
+
+    it "should return an array of Dictionaries" do
+      library.selected_dictionaries.should be_a(Array)
+      library.selected_dictionaries.each do |item|
+        item.should be_a(Solve::Dictionary)
+      end
+    end
+
+    describe "language selction" do
+      describe "with one language selected" do
+        let :american_dictionaries do
+          library.selected_dictionaries(:language => 'american')
+        end
+
+        it "should include english dictionaries" do
+          american_dictionaries.map{|d| d.language}.should include('english')
+        end
+        it "should include american dictionaries" do
+          american_dictionaries.map{|d| d.language}.should include('american')
+        end
+        it "should not include british or canadian dictionaries" do
+          american_dictionaries.map{|d| d.language}.should_not include('british', 'canadian')
+        end
+      end
+      describe "with two language selected" do
+        let :dictionaries do
+          library.selected_dictionaries(:language => ['american', 'canadian'])
+        end
+
+        it "should include english dictionaries" do
+          dictionaries.map{|d| d.language}.should include('english')
+        end
+        it "should include american and canadian dictionaries" do
+          dictionaries.map{|d| d.language}.should include('american', 'canadian')
+        end
+        it "should not include british dictionaries" do
+          dictionaries.map{|d| d.language}.should_not include('british')
+        end
+      end
+
+
+    end
+
+  end
+
 
 end
 
