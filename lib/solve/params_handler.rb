653f2124ad36ed466575dd0893dbb128dc4a2673
Evan Dorn <evan@lrdesign.com>
2012-06-08 15:27:09 -0700

    check validity of input data via regex

--- lib/solve/params_handler.rb
+++ lib/solve/params_handler.rb
@@ -13,7 +13,23 @@ module Solve
     end
 
     def self.check_for_valid_params(params)
+      if params['letters'].nil? or params['letters'] == '' or params['pattern'].nil? or params['pattern'] == ''
+        raise ParamsException.new("Please provide both a pattern and a group of letters.")
+      end
+
       raise ParamsException.new("Too many letters.   Sorry, 8 is the limit for now.") unless params['letters'].length <= LETTERS_LIMIT
+
+      if !letters_only?(params['letters']) or bad_chars?(params['pattern'])
+        raise ParamsException.new("Invalid characters - letters only please!")
+      end
+    end
+
+    def self.bad_chars?(value)
+      value =~ /[^a-zA-Z _-]/
+    end
+
+    def self.letters_only?(value)
+      value =~ /^[a-zA-Z]*$/
     end
 
 
--- spec/params_handler_spec.rb
+++ spec/params_handler_spec.rb
@@ -1,6 +1,42 @@
 require 'solve/params_handler'
 
 describe Solve::ParamsHandler do
+
+  describe "letters_only?" do
+    it "should be true for letters" do
+      Solve::ParamsHandler.letters_only?('abcdefghABCDEFGH').should be_true
+    end
+    it "should be false if it contains punctuation" do
+      Solve::ParamsHandler.letters_only?('abcdefg&&CDEFGH').should be_false
+    end
+    it "should be false if it contains numbers" do
+      Solve::ParamsHandler.letters_only?('abcdefg12CDEFGH').should be_false
+    end
+    it "should be false for letters and spaces" do
+      Solve::ParamsHandler.letters_only?('abcde   ABCDEFGH').should be_false
+    end
+    it "should be false for underscores and hyphens" do
+      Solve::ParamsHandler.letters_only?('abcde__--ABCDEFGH').should be_false
+    end
+  end
+  describe "bad_chars?" do
+    it "should be false for letters" do
+      Solve::ParamsHandler.bad_chars?('abcdefghABCDEFGH').should be_false
+    end
+    it "should be true if it contains punctuation" do
+      Solve::ParamsHandler.bad_chars?('abcdefg&&CDEFGH').should be_true
+    end
+    it "should be true if it contains numbers" do
+      Solve::ParamsHandler.bad_chars?('abcdefg12CDEFGH').should be_true
+    end
+    it "should be false for letters and spaces" do
+      Solve::ParamsHandler.bad_chars?('abcde   ABCDEFGH').should be_false
+    end
+    it "should be false for underscores and hyphens" do
+      Solve::ParamsHandler.bad_chars?('abcde__--ABCDEFGH').should be_false
+    end
+  end
+
   describe 'handle' do
     let :valid_params do
       { 'letters' => 'abcdef', 'pattern' => '  o   ' }
@@ -16,14 +52,38 @@ describe Solve::ParamsHandler do
     end
 
     describe "with a too-long array of letters" do
-
       it "should raise a ParamsException" do
         lambda do
           Solve::ParamsHandler.handle(valid_params.merge!({'letters' => 'abcdefghijklmnopqrstuvwxyz'}))
         end.should raise_exception(Solve::ParamsException)
       end
+    end
+
+    describe "with special characters" do
+      it "should raise a ParamsException" do
+        lambda do
+          Solve::ParamsHandler.handle(valid_params.merge!({'letters' => 'abc13$qg;'}))
+        end.should raise_exception(Solve::ParamsException)
+      end
+    end
+
+    describe "with pattern missing" do
+      it "should raise a ParamsException" do
+        lambda do
+          Solve::ParamsHandler.handle(valid_params.merge!({'pattern' => ''}))
+        end.should raise_exception(Solve::ParamsException)
+      end
 
     end
+    describe "with letters missing" do
+      it "should raise a ParamsException" do
+        lambda do
+          Solve::ParamsHandler.handle(valid_params.merge!({'letters' => ''}))
+        end.should raise_exception(Solve::ParamsException)
+      end
+
+    end
+
   end
 end
 
