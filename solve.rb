a6c535bf8def507e1ce9ad754f996f7ca9fbf3c3
Evan Dorn <evan@lrdesign.com>
2012-06-08 15:09:06 -0700

    added error view for failed conditions

--- solve.rb
+++ solve.rb
@@ -14,17 +14,23 @@ get '/' do
 end
 
 get '/solve' do
-  inputs = Solve::ParamsHandler.handle(params)
-  permutations = Solve::Permuter.pattern_fill(inputs)
+  begin
+    inputs = Solve::ParamsHandler.handle(params)
+    permutations = Solve::Permuter.pattern_fill(inputs)
 
-  results = permutations.each.map do |perm|
-    perm.downcase if LIBRARY.matches_word?(perm.downcase)
-  end.compact
+    results = permutations.each.map do |perm|
+      perm.downcase if LIBRARY.matches_word?(perm.downcase)
+    end.compact
 
-  haml :results, :format => :html5, :locals => {
-    :results => results,
-    :permutations => permutations
-  }.merge!(inputs)
+    haml :results, :format => :html5, :locals => {
+      :results => results,
+      :permutations => permutations
+    }.merge!(inputs)
+  rescue Exception => ex
+    haml :error, :format => :html5, :locals => {
+      :exception => ex
+    }
+  end
 end
 
 get '/application.css' do
